3481cacf239269422b824a3a60527747812f98ff
alejandro <54691090+Ale-cm@users.noreply.github.com>
2020-12-01 00:11:53 -0300

    Prueba:

---
+++ layouts/default.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+
+     <div>
+  <b-navbar toggleable="sm" type="dark" variant="dark">
+    <b-navbar-brand href="#"> <b-link to="flor"><div class="flor"></div></b-link></b-navbar-brand>
+   
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="/index.html">Portfolio</b-nav-item>
+        <b-nav-item href="#" disabled>Miguel Alejandro Cabrera</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+        
+        </b-nav-form>
+
+      
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>Sobre mí</em>
+          </template>
+          <b-dropdown-item href="#">Perfil</b-dropdown-item>
+          <b-dropdown-item href="#">Contacto</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+
+.flor {
+  width: 2em;
+  height: 2em;
+  text-align: center;
+  background-image: url("/loto1.png") ;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+}
+
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container">
+    <div>
+      <div>
+ 
+
+       <h1 class="title">
+      
+            
+      </h1>
+      <div class="links">
+        <a
+          href="https://www.linkedin.com/in/miguel-alejandro-cabrera-a2082515b/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Contacto 
+        </a>
+        <a
+          href="https://github.com/Ale-cm"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          Github
+        </a>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+
+body {
+  margin:0;
+  min-height: 100vh;
+  background-image: url(/imagen1-4.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  
+
+}
+body:before{
+  content:"";
+  width: 100;
+  min-height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+   background: linear-gradient(90deg,rgb(37, 37, 202),rgb(27, 151, 22));
+   }
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+   width: 10em;
+  height: 10em;
+  text-align: left;
+  background-image: url("/logo.png") ;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  
+  
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top:15px;
+}
+</style>
